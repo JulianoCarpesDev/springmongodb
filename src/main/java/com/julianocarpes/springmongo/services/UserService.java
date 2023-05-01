87775251d71504b59c762a73003bc2e984dd156e
JulianoCarpes <julianocarpes.dev@gmail.com>
2023-05-01 08:50:21 -0300

    implemented insert of  user

--- src/main/java/com/julianocarpes/springmongo/services/UserService.java
+++ src/main/java/com/julianocarpes/springmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.julianocarpes.springmongo.domain.User;
+import com.julianocarpes.springmongo.dto.UserDTO;
 import com.julianocarpes.springmongo.repositories.UserRepository;
 import com.julianocarpes.springmongo.services.exception.ObjectNotFoundException;
 
@@ -22,5 +23,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDto(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
